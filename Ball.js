976981de7beb71cbc703c014fc6cd02494c1eb82
ANON4620 <swarnadeep.click@gmail.com>
2023-06-08 12:01:09 +0530

    1. Improved collision system 2. Responsive UI 3. Code cleanup

--- Ball.js
+++ Ball.js
@@ -37,16 +37,73 @@ class Ball {
         const ballRightEdge = this.x + this.radius;
     
         if(ballTopEdge <= 0) {
+            this.y = 0 + this.radius;
             this.speedY = -this.speedY;
         }
-        if(ballLeftEdge <= 0 || ballRightEdge >= canvas.width) {
+        if(ballLeftEdge <= 0) {
+            this.x = 0 + this.radius;
             this.speedX = -this.speedX;
         }
+        if(ballRightEdge >= canvas.width) {
+            this.x = canvas.width - this.radius;
+            this.speedX = -this.speedX;
+        }
+    }
+    
+    checkObjectCollision(object) {
+        const ballTopEdge = this.y - this.radius;
+        const ballBottomEdge = this.y + this.radius;
+        const ballLeftEdge = this.x - this.radius;
+        const ballRightEdge = this.x + this.radius;
+        const ballWidthMidPoint = this.x;
+        
+        const objectTopEdge = object.y;
+        const objectBottomEdge = object.y + object.height;
+        const objectLeftEdge = object.x;
+        const objectRightEdge = object.x + object.width;
+        const objectHeightMidPoint = object.y + (object.height / 2);
+        const objectWidthMidPoint = object.x + (object.width / 2);
+        
+        const dist = ballWidthMidPoint - objectWidthMidPoint; // 10% of object width from the mid point of object
+        
+        // check Top and Bottom Edge of Object
+        if(ballRightEdge >= objectLeftEdge && ballLeftEdge <= objectRightEdge) {
+            if(ballBottomEdge >= objectTopEdge && ballBottomEdge <= objectHeightMidPoint) {
+                const angle = (10 / 100) * dist;
+                this.y = objectTopEdge - this.radius;
+                this.speedX = angle;
+                this.speedY = -this.speedY;
+                return true;
+            }
+            if(ballTopEdge <= objectBottomEdge && ballTopEdge >= objectHeightMidPoint) {
+                const angle = (10 / 100) * dist;
+                this.y = objectBottomEdge + this.radius;
+                this.speedX = angle;
+                this.speedY = -this.speedY;
+                return true;
+            }
+        }
+        
+        // check Left and Right Edge of Object
+        if(ballBottomEdge >= objectTopEdge && ballTopEdge <= objectBottomEdge) {
+            if(ballRightEdge >= objectLeftEdge && ballRightEdge <= objectWidthMidPoint) {
+                this.x = objectLeftEdge - this.radius;
+                this.speedX = -this.speedX;
+                return true;
+            }
+            if(ballLeftEdge <= objectRightEdge && ballLeftEdge >= objectWidthMidPoint) {
+                this.x = objectRightEdge + this.radius;
+                this.speedX = -this.speedX;
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     outOfCanvas() {
         const ballTopEdge = this.y - this.radius;
-
+    
         if(ballTopEdge >= canvas.height) {
             return true;
         }
--- main.js
+++ main.js
@@ -6,37 +6,38 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // Paddle Object
-const paddleWidth = 70;
-const paddleHeight = 12;
-const paddleX = (canvas.width / 2) - (paddleWidth / 2);
+const paddleWidth = (20 / 100) * canvas.width; // 30% of canvas width
+const paddleHeight = (3 / 100) * canvas.height; // 3% of canvas height
+const paddleX = (canvas.width / 2) - (paddleWidth / 2); // position paddle at the center of canvas
 const paddleY = canvas.height - (paddleHeight * 2);
 const paddleColor = "red";
 const paddle = new Paddle(paddleX, paddleY, paddleWidth, paddleHeight, paddleColor);
 
 // Ball Object
-const ballRadius = 10;
-const ballX = paddleX + (paddleWidth / 2);
-const ballY = paddleY - ballRadius - 1;
+const ballRadius = (2 / 100) * canvas.height;
+const ballX = paddleX + (paddleWidth / 2); // position ball at the center of paddle
+const ballY = paddleY - ballRadius - 1; // position ball on top of paddle
 const ballStartAngle = 0 * Math.PI;
 const ballEndAngle = 2 * Math.PI;
 const ballSpeedX = 0;
-const ballSpeedY = 5;
+const ballSpeedY = (50 / 100) * ballRadius; // 50% of ball radius
 const ballColor = "blue";
 const ball = new Ball(ballX, ballY, ballRadius, ballStartAngle, ballEndAngle, ballSpeedX, ballSpeedY, ballColor);
 
 // Brick Objects
 const row = 5;
-const col = 10;
-const brickWidth = (canvas.width / col) - 2;
-const brickHeight = 16;
+const col = 6;
+const brickWidth = (canvas.width / col);
+const brickHeight = (5 / 100) * canvas.height; // 5% of canvas height
 const brickColors = ["violet", "indigo", "blue", "green", "yellow", "orange", "red", "brown", "gold"];
 const borderThickness = 2;
 const borderColor = "black";
 const bricks = [];
 let brickX = (canvas.width / 2) - ((brickWidth * col) / 2);
-let brickY = 2;
+let brickY = 0;
 for(let i = 1; i <= row; i++) {
-    const brickColor = brickColors[Math.floor(Math.random() * brickColors.length)];
+    const randomIndex = Math.floor(Math.random() * brickColors.length);
+    const brickColor = brickColors[randomIndex];
     for(let j = 1; j <= col; j++) {
         bricks.push(new Brick(brickX, brickY, brickWidth, brickHeight, brickColor, borderThickness, borderColor));
         brickX += brickWidth;
@@ -72,8 +73,14 @@ function update() {
     ball.move();
     ball.draw();
     ball.checkEdgeCollision();
-    checkBallAndPaddleCollision();
-    checkBallAndBrickCollision();
+    ball.checkObjectCollision(paddle);
+
+    for(const brick of bricks) {
+        if(ball.checkObjectCollision(brick)) {
+            bricks.splice(bricks.indexOf(brick), 1);
+        }
+    }
+
     if(ball.outOfCanvas()) {
         stop();
         return;
@@ -84,111 +91,6 @@ function update() {
 
 function movePaddleWithMouse(e) {paddle.move(e.clientX)};
 function movePaddleWithTouch(e) {paddle.move(e.touches[0].clientX)};
-
-function checkBallAndPaddleCollision() {
-    function checkPaddleTopOrBottomEdge() {
-        const ballTopEdge = ball.y - ball.radius;
-        const ballBottomEdge = ball.y + ball.radius;
-        const ballLeftEdge = ball.x - ball.radius;
-        const ballRightEdge = ball.x + ball.radius;
-        const ballWidthMidPoint = ball.x;
-
-        const paddleTopEdge = paddle.y;
-        const paddleBottomEdge = paddle.y + paddle.height;
-        const paddleLeftEdge = paddle.x;
-        const paddleRightEdge = paddle.x + paddle.width;
-        const paddleHeightMidPoint = paddle.y + (paddle.height / 2);
-        const paddleWidthMidPoint = paddle.x + (paddle.width / 2);
-
-        const dist = paddleWidthMidPoint - ballWidthMidPoint;
-
-        if(ballRightEdge >= paddleLeftEdge && ballLeftEdge <= paddleRightEdge) {
-            if((ballBottomEdge >= paddleTopEdge && ballBottomEdge <= paddleHeightMidPoint) || (ballTopEdge <= paddleBottomEdge && ballTopEdge >= paddleHeightMidPoint)) {
-                const angle = dist * 0.2;
-                ball.speedX = angle;
-                ball.speedY = -ball.speedY;
-            }
-        }
-    }
-
-    function checkPaddleLeftOrRightEdge() {
-        const ballTopEdge = ball.y - ball.radius;
-        const ballBottomEdge = ball.y + ball.radius;
-        const ballLeftEdge = ball.x - ball.radius;
-        const ballRightEdge = ball.x + ball.radius;
-
-        const paddleTopEdge = paddle.y;
-        const paddleBottomEdge = paddle.y + paddle.height;
-        const paddleLeftEdge = paddle.x;
-        const paddleRightEdge = paddle.x + paddle.width;
-        const paddleWidthMidPoint = paddle.x + (paddle.width / 2);
-
-        if(ballBottomEdge >= paddleTopEdge && ballTopEdge <= paddleBottomEdge) {
-            if((ballRightEdge >= paddleLeftEdge && ballRightEdge <= paddleWidthMidPoint) || (ballLeftEdge <= paddleRightEdge && ballLeftEdge >= paddleWidthMidPoint)) {
-                ball.speedX = -ball.speedX;
-            }
-        }
-    }
-
-    checkPaddleTopOrBottomEdge();
-    checkPaddleLeftOrRightEdge();
-}
-
-function checkBallAndBrickCollision() {
-    function checkBrickTopOrBottomEdge() {
-        for(const brick of bricks) {
-            const ballTopEdge = ball.y - ball.radius;
-            const ballBottomEdge = ball.y + ball.radius;
-            const ballLeftEdge = ball.x - ball.radius;
-            const ballRightEdge = ball.x + ball.radius;
-            const ballWidthMidPoint = ball.x;
-
-            const brickTopEdge = brick.y;
-            const brickBottomEdge = brick.y + brick.height;
-            const brickLeftEdge = brick.x;
-            const brickRightEdge = brick.x + brick.width;
-            const brickHeightMidPoint = brick.y + (brick.height / 2);
-            const brickWidthMidPoint = brick.x + (brick.width / 2);
-
-            const dist = brickWidthMidPoint - ballWidthMidPoint;
-
-            if(ballRightEdge >= brickLeftEdge && ballLeftEdge <= brickRightEdge) {
-                if((ballBottomEdge >= brickTopEdge && ballBottomEdge <= brickHeightMidPoint) || (ballTopEdge <= brickBottomEdge && ballTopEdge >= brickHeightMidPoint)) {
-                    bricks.splice(bricks.indexOf(brick), 1);
-                    const angle = dist * 0.2;
-                    ball.speedX = angle;
-                    ball.speedY = -ball.speedY;
-                }
-            }
-        }
-    }
-
-    function checkBrickLeftOrRightEdge() {
-        for(const brick of bricks) {
-            const ballTopEdge = ball.y - ball.radius;
-            const ballBottomEdge = ball.y + ball.radius;
-            const ballLeftEdge = ball.x - ball.radius;
-            const ballRightEdge = ball.x + ball.radius;
-
-            const brickTopEdge = brick.y;
-            const brickBottomEdge = brick.y + brick.height;
-            const brickLeftEdge = brick.x;
-            const brickRightEdge = brick.x + brick.width;
-            const brickWidthMidPoint = brick.x + (brick.width / 2);
-
-            if(ballBottomEdge >= brickTopEdge && ballTopEdge <= brickBottomEdge) {
-                if((ballRightEdge >= brickLeftEdge && ballRightEdge <= brickWidthMidPoint) || (ballLeftEdge <= brickRightEdge && ballLeftEdge >= brickWidthMidPoint)) {
-                    bricks.splice(bricks.indexOf(brick), 1);
-                    ball.speedX = -ball.speedX;
-                }
-            }
-        }
-    }
-
-    checkBrickTopOrBottomEdge();
-    checkBrickLeftOrRightEdge();
-}
-
 
 let pause = false;
 document.addEventListener('keydown' , (e) => {
